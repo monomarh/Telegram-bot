297f3e9673851a46c2577b68b886fd8f42e9e19b
Monomarh <r.monomarh@gmail.com>
2019-04-19 15:37:28 +0300

    [fix][back-end]Fix bugs

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -31,12 +31,13 @@ class IndexController extends AbstractController
 
         $botman = BotManFactory::create($config);
 
-        $botman->hears('/hello', static function(BotMan $bot) {
+        $botman->hears('hello', static function(BotMan $bot) {
             $bot->reply('Hello yourself.');
         });
 
         $botman->hears('call me {name}', static function(BotMan $bot, string $name) {
             $bot->reply(sprintf('Hello %s.', $name));
+            $bot->reply(sprintf('Hello %s.', $bot->getUser()));
         });
 
         $botman->hears('i live in {city}', static function(BotMan $bot, string $city) {
@@ -46,6 +47,19 @@ class IndexController extends AbstractController
         $botman->hears('i was born {birthday}', static function(BotMan $bot, string $birthday) {
             $birthdayDate = new \DateTime($birthday);
             $bot->reply(sprintf('You\'re %s years old.', $birthdayDate->diff(new \DateTime())->format('%Y')));
+        });
+
+        $botman->hears('i was born {birthday}', static function(BotMan $bot, string $birthday) {
+            $birthdayDate = new \DateTime($birthday);
+            $bot->reply(sprintf('You\'re %s years old.', $birthdayDate->diff(new \DateTime())->format('%Y')));
+        });
+
+        $botman->hears('help', static function(BotMan $bot) {
+            $commandList =
+                'help - command list with description' . PHP_EOL .
+                'call me "YOUR NAME" - enter your name instead of "YOUR NAME", after that the bot will call you in a new way' . PHP_EOL .
+                'i live in "YOUR CITY" - enter your city instead of "YOUR CITY", after that the bot will send the weather for this city' . PHP_EOL;
+            $bot->reply(sprintf('You\'re %s years old.', $commandList);
         });
 
         $botman->fallback(static function(BotMan $bot) {
