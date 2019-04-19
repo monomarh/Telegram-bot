8ed2487d8e52cc9bae5b21e62ab7a531ead32ef9
Monomarh <r.monomarh@gmail.com>
2019-04-19 15:18:27 +0300

    [feat][back-end]Add new command

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -35,6 +35,19 @@ class IndexController extends AbstractController
             $bot->reply('Hello yourself.');
         });
 
+        $botman->hears('call me {name}', static function(BotMan $bot, string $name) {
+            $bot->reply(sprintf('Hello %s.', $name));
+        });
+
+        $botman->hears('i live in {city}', static function(BotMan $bot, string $city) {
+            $bot->reply(sprintf('Weather in %s as ass.', $city));
+        });
+
+        $botman->hears('i was born {$birthday}', static function(BotMan $bot, string $birthday) {
+            $birthdayDate = new \DateTime($birthday);
+            $bot->reply(sprintf('You\'re %s years old.', $birthdayDate->diff(new \DateTime())->format('%Y')));
+        });
+
         $botman->fallback(static function(BotMan $bot) {
             $bot->reply('Sorry, I did not understand these commands. Type help for command list');
         });
