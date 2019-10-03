6a9739200e0f8f4981dfada86cbc0714d948af36
Ruslan, Kravchuk <r.kravchuk@itransition.com>
2019-10-03 17:06:11 +0300

    Change routing to Route

--- src/Controller/AssigmentController.php
+++ src/Controller/AssigmentController.php
@@ -3,10 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AssigmentController extends AbstractController
 {
+    /**
+     * @Route("/assigment", name="assigment")
+     *
+     * @return Response
+     */
     public function indexAction()
     {
         return $this->render('assigment/index.html.twig', [
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,6 +13,7 @@ use \DateTime;
 use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
@@ -33,6 +34,8 @@ class IndexController extends AbstractController
     }
 
     /**
+     * @Route("/", name="index")
+     *
      * @return Response
      */
     public function indexAction(): Response
