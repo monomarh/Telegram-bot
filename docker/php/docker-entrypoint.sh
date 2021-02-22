3fe3e9b99f6f10dbc201fac82e92940b5431fb60
monomarh <r.monomarh@gmail.com>
2021-02-22 20:01:32 +0300

    Update docker installation process

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 set -e
 
-if [[ $APP_ENV = 'dev' ]]; then
-  composer install --prefer-dist
-else
+if [[ $APP_ENV != 'test' ]]; then
+  echo >&2 "Waiting for database connection..."
+  until nc -z ${PSQL_HOST} ${PSQL_PORT}; do
+      sleep 2
+  done
+fi
+
+if [[ $APP_ENV = 'prod' ]]; then
   composer install --prefer-dist --no-dev
+else
+  composer install --prefer-dist
 fi
 
 exec docker-php-entrypoint "$@"
